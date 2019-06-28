5396219686bb3e9d5925e4b6f2ebaa03406bd676
sw-yx <swyx@dontemail.me>
2019-06-28 11:16:35 -0400

    init

--- functions/isNetlify/isNetlify.js
+++ functions/isNetlify/isNetlify.js
@@ -1,18 +1,19 @@
 /* eslint-disable */
 const fetch = require("node-fetch")
 exports.handler = async function(event, context) {
-  const path = event.queryStringParameters.path || null
+  let path = event.queryStringParameters.path || null
   if (!path)
     return {
       statusCode: 400,
       body: "you must specify a path parameter"
     }
+  if (path.startsWith("https://")) path = path.slice(8)
+  if (path.startsWith("http://")) path = path.slice(7)
   try {
     const server = await fetch(`https://${path}`).then((res) => {
-      return {
-        server: res.headers.get("server"),
-        nfrid: res.headers.get("x-nf-request-id")
-      }
+      let obj = {}
+      res.headers.forEach((v, k) => (obj[k] = v))
+      return obj
     })
 
     return {
--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,19 @@
 <script>
   export let name;
-  export let header = "potato";
+  export let nfrid = "x-nf-request-id";
+  export let headers = "potato";
   export let path = document.location.pathname.slice(1);
   if (path.length) {
-    // there's a path to try!
     fetch(`/.netlify/functions/isNetlify?path=${path}`)
       .then(res => res.json())
       .then(x => {
-        header = x;
-        console.log({ header });
+        headers = x;
+        console.log({ headers });
       });
   }
-  export let isNetlify = !!header.nfrid;
-  // export let isLoading = !!(isNetlify && header);
+  export let isNetlify = !!headers[nfrid]; // is not properly reactive :(
+  export let server = headers["server"];
+  // export let isLoading = !!(isNetlify && headers);
 </script>
 
 <style>
@@ -27,13 +28,51 @@
     font-weight: bold;
     color: red;
   }
+  .serverheader {
+    font-weight: bold;
+    color: purple;
+  }
+  blockquote {
+    background-color: lightyellow;
+    border-radius: 10px;
+    padding: 10px;
+  }
+  pre {
+    background-color: lightgray;
+    border-radius: 10px;
+    padding: 10px;
+  }
 </style>
 
 <h1>Is This Netlify?</h1>
-<h3>Checking Domain: {path}</h3>
+<h3>Checking Domain: {path || 'no domain specified!'}</h3>
 <p>
   Is This Netlify:
-  <span class={header.nfrid ? 'success' : 'fail'}>
-     {header.nfrid ? 'Yes' : 'No'}
+  <span class={headers[nfrid] ? 'success' : 'fail'}>
+     {headers[nfrid] ? 'Yup' : 'Nope'}
   </span>
 </p>
+
+<p>
+  Server Header:
+  <span class="serverheader"> {headers['server']} </span>
+</p>
+
+<blockquote>
+  <p>
+    <strong>how to use:</strong>
+  </p>
+  prepend any url with 'https://is-this.netlify.com', e.g.
+  <a href="https://is-this.netlify.com/barstoolsports.com/anything.html">
+    https://is-this.netlify.com/barstoolsports.com/anything.html
+  </a>
+  .
+</blockquote>
+
+<h3>Header Dump</h3>
+<pre> {JSON.stringify(headers, null, 2)} </pre>
+
+<h3>
+  <a href="https://github.com/sw-yx/is-this-netlify">View Source</a>
+  for this project
+</h3>
